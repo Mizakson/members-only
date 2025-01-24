cf2a2d496d06ac5a851663ceee912a5e585d4b58
Mizakson <mizakson.dev@gmail.com>
2025-01-24 18:00:06 -0500

    Confirm response sent, show homepage message

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,6 +1,9 @@
+const { Render } = require("ejs")
+
 async function indexPageGet(req, res) {
     // query here
     console.log("db query here...")
+    res.send("HOMEPAGE...")
 }
 
 module.exports = {
