4d41e9a1d711c0ef55ddd16d21c8912c602e52a9
Mizakson <mizakson.dev@gmail.com>
2025-01-24 17:45:10 -0500

    Create pool.js

---
+++ db/pool.js
@@ -0,0 +1,13 @@
+const { Pool } = require("pg")
+
+module.exports = new Pool({
+    connectionString: process.env.DATABASE_URL,
+})
+
+// module.exports = new Pool({
+//     host: process.env.HOST,
+//     user: process.env.USER,
+//     database: process.env.DATABASE,
+//     password: process.env.PASSWORD,
+//     port: process.env.PORT,
+// })
