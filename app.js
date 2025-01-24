d4b5c7a7018c054676038067552890d2ce1d78ff
Mizakson <mizakson.dev@gmail.com>
2025-01-24 12:39:20 -0500

    Create app.js
    Server listening to port successfully

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const path = require("path")
+const app = express()
+
+require("dotenv").config()
+
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "ejs")
+app.use(express.urlencoded({ extended: true }))
+
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath))
+
+app.get("/", (req, res) => {
+    res.send("TESTING...")
+})
+
+const PORT = 3000
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`listening on http://localhost:${PORT}`)
+})
