ed0a412d36236c35c10c72e070c35a5824a5e954
Mizakson <mizakson.dev@gmail.com>
2025-01-29 11:15:43 -0500

    Refactor export statment syntax
    module.exports => exports.funcName

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,13 +1,9 @@
 const { Render } = require("ejs")
 const db = require("../db/queries")
 
-async function indexPageGet(req, res) {
+exports.indexPageGet = async (req, res) => {
     // query here
     const allUsers = await db.getAllUsers()
     console.log(allUsers)
     res.send("HOMEPAGE...")
-}
-
-module.exports = {
-    indexPageGet
 }
