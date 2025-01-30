8deaac2fbf89504217ab3ea4fc130f026584d855
Mizakson <mizakson.dev@gmail.com>
2025-01-30 12:57:25 -0500

    Delete members files, move to initial sign-up code

--- routes/membersRouter.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express")
-const membersRouter = Router()
-const membersController = require("../controllers/membersController")
-
-membersRouter.get("/", membersController.membersPageGet)
-membersRouter.post("/", membersController.membersPagePost)
-
-module.exports = membersRouter
