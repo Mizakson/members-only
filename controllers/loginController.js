c4effe65dcef006da3ab54a47eb29e7f1a7adf01
Mizakson <mizakson.dev@gmail.com>
2025-02-03 11:30:43 -0500

    Edit formatting, delete logOut get method

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,15 +1,6 @@
-exports.logInPostReq = () => (
+exports.logInPostReq = () => {
     passport.authenticate("local", {
       successRedirect: "/",
       failureRedirect: "/"
     })
-  ); 
-  
-exports.logOutGetReq = (req, res, next) => {
-  req.logout((err) => {
-    if (err) {
-      return next(err);
-    }
-    res.redirect("/");
-  });
-}
+}; 
