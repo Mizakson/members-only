606e4ba093f0148418fc0ee0300c2abb2a8c2cc0
Mizakson <mizakson.dev@gmail.com>
2025-02-03 11:23:14 -0500

    Add initial localStrategy

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-const express = require("express")
-const path = require("path")
+const path = require("node:path");
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require("bcryptjs")
 const app = express()
 
 require("dotenv").config()
@@ -11,12 +15,56 @@ app.use(express.urlencoded({ extended: true }))
 const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath))
 
+app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    next();
+  });
+
 const indexRouter = require("./routes/indexRouter")
 const signUpRouter = require("./routes/signUpRouter")
 
 app.use("/", indexRouter)
 app.use("/sign-up", signUpRouter)
 
+passport.use(
+    new LocalStrategy(async (username, password, done) => {
+      try {
+        const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
+        const user = rows[0];
+  
+        if (!user) {
+          return done(null, false, { message: "Incorrect username" });
+        }
+        const match = await bcrypt.compare(password, user.password);
+        if (!match) {
+          return done(null, false, { message: "Incorrect password" });
+        }
+        return done(null, user);
+      } catch(err) {
+        return done(err);
+      }
+    })
+);
+  
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+  
+passport.deserializeUser(async (id, done) => {
+    try {
+      const { rows } = await pool.query("SELECT * FROM users WHERE user_id = $1", [id]);
+      const user = rows[0];
+  
+      done(null, user);
+    } catch(err) {
+      done(err);
+    }
+});
+  
 const PORT = 3000
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`listening on http://localhost:${PORT}`)
