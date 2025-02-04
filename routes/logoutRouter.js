722bb4d0724cf3ef8355f4c00d1a53bbcfc0f047
Mizakson <mizakson.dev@gmail.com>
2025-02-04 15:20:06 -0500

    Reset routes, delete router files

--- routes/logoutRouter.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require("express")
-const logOutRouter = Router()
-const logOutController = require("../controllers/logoutController")
-
-logOutRouter.get("/", logOutController.logOutGetReq)
-
-module.exports = logOutRouter
--- routes/signUpRouter.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express")
-const signUpRouter = Router()
-const signUpController = require("../controllers/signUpController")
-
-signUpRouter.get("/", signUpController.signUpPageGet)
-signUpRouter.post("/", signUpController.signUpPagePost)
-
-module.exports = signUpRouter
