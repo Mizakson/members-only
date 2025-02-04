0f3c0f23be5e369cf8a6a4c918891176121ed79b
Mizakson <mizakson.dev@gmail.com>
2025-02-04 10:20:02 -0500

    Edit express session options object

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(session({
     }),
     secret: "yo", 
     resave: false, 
-    saveUninitialized: true,
+    saveUninitialized: false,
     cookie: { maxAge: 30 * 24 * 60 * 60 * 1000} // 30 days
 }));
 app.use(passport.session());
