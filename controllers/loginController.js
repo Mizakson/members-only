c44bf8da275f2dae0eef9fc4f508a3fec63760a3
Mizakson <mizakson.dev@gmail.com>
2025-02-04 15:43:06 -0500

    Render login page, still work on auth w/ passport

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,8 +1,13 @@
 const passport = require("passport")
 
+exports.logInGetReq = (req, res) => {
+  res.render("login")
+}
+
 exports.logInPostReq = (req, res) => {
     passport.authenticate("local", {
       successRedirect: "/",
       failureRedirect: "/", // incorrect login page here?
     })
+    return
 }; 
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -3,10 +3,13 @@ const indexRouter = Router()
 
 const { indexPageGet } = require("../controllers/indexController")
 const { signUpPageGet, signUpPagePost } = require("../controllers/signUpController")
+const { logInGetReq, logInPostReq } = require("../controllers/loginController")
 
 indexRouter.get("/", indexPageGet)
 indexRouter.get("/sign-up", signUpPageGet)
+indexRouter.get("/log-in", logInGetReq)
 
 indexRouter.post("/sign-up", signUpPagePost)
+indexRouter.post("/log-in", logInPostReq)
 
 module.exports = indexRouter
