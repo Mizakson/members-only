07b8224d003d2d2ae8cf2aae63a030765ff0a36b
Mizakson <mizakson.dev@gmail.com>
2025-02-04 10:54:33 -0500

    Edit require statements

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ require("dotenv").config()
 const express = require("express");
 const session = require("express-session");
 const pgSession = require("connect-pg-simple")(session)
-const passport = require("passport");
-const LocalStrategy = require('passport-local').Strategy;
-const bcrypt = require("bcryptjs")
 const pool = require("./db/pool");
 
 const app = express()
