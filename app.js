16d8f72ae0a4f6d07b3b6d2e5ff3eb6548e33944
Mizakson <mizakson.dev@gmail.com>
2025-02-04 10:24:55 -0500

    Edit cookie & secret options - express session obj

--- app.js
+++ app.js
@@ -20,10 +20,10 @@ app.use(session({
     store: new (pgSession)({
         pool: pool,
     }),
-    secret: "yo", 
+    secret: process.env.COOKIE_SECRET, 
     resave: false, 
     saveUninitialized: false,
-    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000} // 30 days
+    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 } // 30 days
 }));
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
