b00882ab41b75f5a8d5cb302c1e08913beb1ef19
Mizakson <mizakson.dev@gmail.com>
2025-02-19 13:07:37 -0500

    Delete all project files but app.js

--- app.js
+++ app.js
@@ -1,37 +1,6 @@
-const path = require("node:path");
-require("dotenv").config()
-const express = require("express");
-const session = require("express-session");
-const pgSession = require("connect-pg-simple")(session)
-const passport = require("passport")
-const pool = require("./db/pool");
-const indexRouter = require("./routes/indexRouter");
 
-const app = express()
 
-app.set("views", path.join(__dirname, "views"))
-app.set("view engine", "ejs")
-
-const assetsPath = path.join(__dirname, "public");
-app.use(express.static(assetsPath))
-
-app.use(session({
-    store: new (pgSession)({
-        pool: pool,
-    }),
-    secret: process.env.COOKIE_SECRET, 
-    resave: false, 
-    saveUninitialized: true,
-    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 } // 30 days
-}));
-app.use(passport.session());
-app.use(express.urlencoded({ extended: false }));
-
-require("./config/passport")
-
-app.use(indexRouter)
-
-const PORT = 3000
-app.listen(PORT, "0.0.0.0", () => {
-    console.log(`listening on http://localhost:${PORT}`)
-})
+// const PORT = 3000
+// app.listen(PORT, "0.0.0.0", () => {
+//     console.log(`listening on http://localhost:${PORT}`)
+// })
--- controllers/indexController.js
+++
@@ -1,8 +0,0 @@
-const { Render } = require("ejs")
-const db = require("../db/queries")
-
-exports.indexPageGet = async (req, res) => {
-    const allUsers = await db.getAllUsers()
-    console.log(allUsers)
-    res.render("index");
-}
