2f4977c5c2d4703e5e6bcbfc66787ac176e42ed9
Mizakson <mizakson.dev@gmail.com>
2025-02-19 13:25:24 -0500

    Add require statements to app.js

--- app.js
+++ app.js
@@ -1,6 +1,36 @@
+require("dotenv").config();
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const bcrypt = require("bcryptjs");
+const localStrategy = require("passport-local").Strategy;
+const pool = require("./db/pool");
+const queries = require("./db/queries");
+const app = express();
+
+// routers here
 
 
-// const PORT = 3000
-// app.listen(PORT, "0.0.0.0", () => {
-//     console.log(`listening on http://localhost:${PORT}`)
-// })
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+
+app.use(session({
+    secret: "tacos",
+    resave: false,
+    saveUninitialized: false,
+}));
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+// localStrategy here
+
+
+// requests here
+
+const PORT = 3000
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`listening on http://localhost:${PORT}`)
+})
