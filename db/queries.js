59e9c2db89cb08965371708afb7bd9c76e292eb0
Mizakson <mizakson.dev@gmail.com>
2025-02-20 15:25:10 -0500

    Change membership_status to int from bool type

--- db/queries.js
+++ db/queries.js
@@ -1,5 +1,7 @@
 const pool = require("./pool")
 
+// status -- 0 (non-member), 1 (member), 2 (admin)
+
 exports.getAllUsers = async () => {
     const { rows } = await pool.query("SELECT * FROM users")
     return rows
@@ -17,14 +19,20 @@ exports.getAllUsers = async () => {
 
 exports.addUser = async (firstName, lastName, username, password) => {
     await pool.query("INSERT INTO users (first_name, last_name, username, password, membership_status) VALUES ($1, $2, $3, $4, $5)", 
-        [firstName, lastName, username, password, "FALSE"])
-}
+        [firstName, lastName, username, password, 0]);
+};
 
 exports.getUserById = async (userId) => {
-    const { rows } = await pool.query("SELECT * FROM users WHERE user_id = $1", [userId])
-    return rows
-}
+    const { rows } = await pool.query("SELECT * FROM users WHERE user_id = $1", [userId]);
+    return rows;
+};
 
 exports.addMember = async (user_id) => {
-    await pool.query("UPDATE users SET membership_status = true WHERE id = $1", [user_id])
-}
+    const { rows } = await pool.query("UPDATE users SET membership_status = 1 WHERE id = $1", [user_id]);
+    return rows;
+};
+
+exports.makeMemberAdmin = async (user_id) => {
+    const { rows } = await pool.query("UPDATE users SET membership_status = 2 WHERE id = $1", [user_id]);
+    return rows;
+};
